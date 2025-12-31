fd58471f66b83ae55f6a36cdbd886442db76d8ee
Jeff Wilson <rjp.wilson@gmail.com>
2025-12-30 20:48:14 -0500

    CH4 Lesson 3: Agent can repair code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, argparse
+import sys, argparse
 from config import GEMINI_API_KEY
 from google import genai
 from google.genai import types
@@ -10,46 +10,68 @@ from functions.run_python_file import available_functions as rpf_functions
 from functions.write_file import available_functions as wf_functions
 from functions.call_function import call_function
 
-def call_ai(args, client):
-    
+def call_ai(args, client, max_calls=20):
+
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    received_response = False
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash", 
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[gfi_functions, gfc_functions, rpf_functions, wf_functions], 
-            system_instruction=system_prompt)
-        )
+    for _ in range(max_calls):
     
-    if not response.usage_metadata:
-        raise RuntimeError("Gemini did not respond")
+        response = client.models.generate_content(
+            model="gemini-2.5-flash", 
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[gfi_functions, gfc_functions, rpf_functions, wf_functions], 
+                system_instruction=system_prompt)
+            )
+        
+        if not response.usage_metadata:
+            raise RuntimeError("Gemini did not respond")
 
-    prompt_token_count = response.usage_metadata.prompt_token_count
-    candidates_token_count = response.usage_metadata.candidates_token_count
+        prompt_token_count = response.usage_metadata.prompt_token_count
+        candidates_token_count = response.usage_metadata.candidates_token_count
 
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {prompt_token_count}")
-        print(f"Response tokens: {candidates_token_count}")
+        # add candidate responses to conversation
+        if len(response.candidates) > 0:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
 
-    # f"Calling function: {function_call.name}({function_call.args})"
-    if response.function_calls and len(response.function_calls) > 0:
-        function_results = []
-        for function_call in response.function_calls:
-            # print(f"Calling function: {function_call.name}({function_call.args})")
-            function_call_result = call_function(function_call, args.verbose)
-            if not function_call_result.parts or len(function_call_result.parts) == 0:
-                raise Exception("Unexpected result from calling local function")
-            if function_call_result.parts[0].function_response == None:
-                raise Exception("No response from local function")
-            if function_call_result.parts[0].function_response.response == None:
-                raise Exception("No response found in function_response")
-            function_results.append(function_call_result.parts[0])
-            if args.verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
-    else:
-        print(f"Response:\n{response.text}")
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {prompt_token_count}")
+            print(f"Response tokens: {candidates_token_count}")
+
+        if response.function_calls and len(response.function_calls) > 0:
+
+            function_results = []
+
+            for function_call in response.function_calls:
+
+                function_call_result = call_function(function_call, args.verbose)
+
+                if not function_call_result.parts or len(function_call_result.parts) == 0:
+                    raise Exception("Unexpected result from calling local function")
+                if function_call_result.parts[0].function_response == None:
+                    raise Exception("No response from local function")
+                if function_call_result.parts[0].function_response.response == None:
+                    raise Exception("No response found in function_response")
+                
+                function_results.append(function_call_result.parts[0])
+
+                if args.verbose:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+
+            # retain function call results as user parts in the conversation
+            messages.append(types.Content(role="user", parts=function_results))
+
+        else:
+            print(f"Response:\n{response.text}")
+            received_response = True
+            break # break out of the for loop here
+    
+    if not received_response:
+        print("Interaction ended without a response.")
+        sys.exit(1)
 
 def main():
 
@@ -60,8 +82,7 @@ def main():
 
     client = genai.Client(api_key=GEMINI_API_KEY)
 
-    call_ai(args, client)
-
+    call_ai(args, client, 20)
 
 if __name__ == "__main__":
     main()
